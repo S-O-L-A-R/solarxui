3bea92b78315e438893816986debe30a5e1d1332
voraton.l <voraton@wongnai.com>
2019-09-06 00:00:10 +0700

    Make it display hex

--- src/colors/stories.tsx
+++ src/colors/stories.tsx
@@ -4,12 +4,20 @@ import { number, select } from '@storybook/addon-knobs'
 import styled, { css } from 'styled-components'
 import { primary, danger, gray } from '.'
 
-const Color = styled.div`
+const Color = styled(
+	({ color, className }: { color: string; className?: string }) => (
+		<div className={className}>{color}</div>
+	),
+)`
 	width: 100vw;
 	height: 100vh;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	${({ color }: { color: string }) => css`
 		background: ${color};
-	`}
+	`};
 `
 
 storiesOf('Style', module).add('colors', () => (
