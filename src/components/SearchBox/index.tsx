1cdafa0be32465d4d812e9f8a394e93906a06d99
admin <phasin@wongnai.com>
2019-09-11 00:37:26 +0700

    refactor code

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,37 +1,12 @@
-import React, { HtmlHTMLAttributes, HTMLAttributes } from 'react'
-import styled from 'styled-components'
+import React, { HTMLAttributes } from 'react'
 import { gray } from '../../colors'
 import SearchIcon from '../../icons/search'
+import { Container } from './styled'
 
 interface Props extends HTMLAttributes<HTMLInputElement> {
 	placeholder: string
 	className?: string
 }
-
-const SEARCH_BOX_HEIGHT = '50px'
-
-const Container = styled.div`
-	width: 100%;
-	height: ${SEARCH_BOX_HEIGHT};
-	box-sizing: border-box;
-	padding: 12px 16px;
-	box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
-	border-radius: 16px;
-	display: flex;
-	align-items: center;
-	input {
-		outline: none;
-		border: none;
-		font-size: 18px;
-		width: 100%;
-		::placeholder {
-			color: ${gray[2]};
-		}
-	}
-	> *:not(:last-child) {
-		margin-right: 8px;
-	}
-`
 
 const SearchBox = ({ placeholder, className, ...props }: Props) => {
 	return (
