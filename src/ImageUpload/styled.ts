1bfc688ffa471d3a24f14001b6f531679e6ab5f2
Pipatpol Tanavongchinda <pipatpol.t@ku.th>
2019-09-14 00:55:39 +0700

    :feat Add Image Upload Component
    * Add ImageUpload Component
    
    * Remove modal close button
    
    * Change color
    
    * add input
    
    * Change text props to required type

---
+++ src/ImageUpload/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { gray } from '../colors'
+
+export const StyledImageUpload = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 24px;
+	height: 210px;
+`
+
+export const StyledImageInput = styled.input`
+	display: none;
+`
+
+export const StyledImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`
